da5a53eb98a9cde2abde07c56cef7a3aa049f92e
saad office <saadoffice@mail.com>
2022-03-05 21:52:04 +0500

    Generic section heading, text styling set

--- src/components/homepage/GamePlay.js
+++ src/components/homepage/GamePlay.js
@@ -9,15 +9,14 @@ export default function GamePlay({ history }) {
     return(
         <>
         <Container>
-            <Row className="gamePlayRow">
+            <Row className="sectionRow gamePlayRow">
                 <Col lg={12}>
-                    <h1 className="gamePlayHeading">Game Play</h1>
-                    <p className="gamePlayText">Metal Roads is a Free-to-play and Play-to-earn blockchain-based action game consisting of stunts, racing and battle modes. In the games players can use NFT Trucks & equipment and go to war, race & stunts with other players in an ever-expanding gaming world where the possibilities are limitless.</p>
-                    <Image className="gamePlayImg" loading="lazy" src={GamePlayImage} alt="game play image" fluid={true} />
+                    <h1 className="sectionHeading gamePlayHeading">Game Play</h1>
+                    <p className="sectionText gamePlayText">Metal Roads is a Free-to-play and Play-to-earn blockchain-based action game consisting of stunts, racing and battle modes. In the games players can use NFT Trucks & equipment and go to war, race & stunts with other players in an ever-expanding gaming world where the possibilities are limitless.</p>
+                    <Image className="sectionImage gamePlayImg" loading="lazy" src={GamePlayImage} alt="game play image" fluid={true} />
                 </Col>
             </Row>
         </Container>
-            
         </>
     )
 
