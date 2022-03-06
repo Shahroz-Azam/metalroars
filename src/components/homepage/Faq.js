8b73716119547cd694c7e2a2f70985e261dfff33
saad office <saadoffice@mail.com>
2022-03-06 21:43:41 +0500

    faqs added

--- src/components/homepage/Faq.js
+++ src/components/homepage/Faq.js
@@ -4,41 +4,106 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Accordion from 'react-bootstrap/Accordion'
 export default function Faq() {
-    return(
+    return (
         <>
             <Container>
-            <Row className="sectionRow faqRow">
+                <Row className="sectionRow faqRow">
                     <Col lg={12}>
                         <h1 className="sectionHeading faqHeading">FAQS</h1>
                     </Col>
                     <Col lg={12}>
                         <Accordion>
                             <Accordion.Item eventKey="0">
-                                <Accordion.Header>Can you provide us with a brief introduction to Super Vet? </Accordion.Header>
+                                <Accordion.Header>Can you provide us with a brief introduction to Metal Roars? </Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua.
+                                    <p>In Metal Roars, you can own and drive your own 3d monster truck. It is the first-ever NFT-based game featuring 3d monster trucks. As a truck driver, you must fight against other truckers in a post-apocalyptic world in order to survive. Having such powerful and resilient vehicles is the best way to show off your stunts and driving skills.
+                                    </p>
+                                    <p>Metal Roars is one of the first games released on Binance smart chain. As the world is moving towards blockchain, we decided to be a part of it in a unique way. The game is more exciting and engaging as players will be able to buy and sell game assets on the open market.</p>
+                                    <p>There are a variety of trucks in Metal Roars, each with unique abilities. A variety of different parts can be added to your truck to help you defeat your opponents. You'll be entertained for hours with this game's action and excitement.</p>
+                                    <p>You will be put to the test in this game if you have stunting talent, fast driving skills, and Monster Truck driving experience.</p>
+
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item eventKey="1">
-                                <Accordion.Header>What is the gameplay like briefly?</Accordion.Header>
+                                <Accordion.Header>With so many new NFT games emerging, what do Metal Roars offer to attract new gamers into the space and hold on to veteran players?</Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua.
+                                    <p>You can not compare Metal roars with any other game. The way this game is designed, the graphics, the environment, everything is on a whole new level. Not only do we offer great graphics but also a Truck driving experience that you will never forget.
+                                    </p>
+                                    <p>In Metal Roars, gamers can buy and sell game assets on the open market. This makes the game more exciting and engaging as players will be able to trade their assets with others.
+                                    </p>
+                                    <p>New gamers tend to play games that can give them rewards. In Metal Roars, gamers can get rewards in the form of coins that can be used to buy new trucks and parts for their truck. This will keep them coming back for more.
+                                    </p>
+                                    <p>Veteran players are important to us, and we want to keep them happy. They can expect exciting updates with new trucks, parts, maps, and game modes. We will also be listening to their feedback and implementing changes that they feel will make the game more enjoyable.</p>
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item eventKey="2">
-                                <Accordion.Header>Where can I learn more about the game?</Accordion.Header>
+                                <Accordion.Header>Do you have a strategy in place to maintain Metal Roars token at a reasonable price and prevent manipulation by whales?</Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua.
+                                    <p>Yes, we have a complete strategy in place to maintain the Metal Roars token at a reasonable price. We will be releasing new game updates, which will keep the gamers engaged. We will also be working on listing the Metal Roars token on more exchanges so that more people can have access to it.
+                                    </p>
+                                    <p>We believe that our strategy will prevent manipulation by whales.</p>
+                                    <p>We have taken the following precautionary measure to save our token from manipulation.</p>
+                                    <ol>
+                                        <li>Listing on multiple exchanges</li>
+                                        <li>Releasing frequent game updates</li>
+                                        <li>Working with the community to get their feedback and incorporating changes based on that feedback.</li>
+                                        <li>Partnership with other games and platforms.
+                                        </li>
+                                        <li>Maintaining a reasonable chunk of tokens for the company</li>
+                                    </ol>
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item eventKey="3">
+                                <Accordion.Header>There remains a large gap between traditional gamers and blockchain gamers. What more can Metal Roars do to bridge this gap?
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    <p>We understand that there is a large gap between traditional gamers and blockchain gamers. We believe that Metal Roars can bridge this gap by providing an exciting experience that traditional gamers are looking for. We will also be working with other games and platforms to promote collaboration and interoperability.</p>
+                                    <p>We believe that this will help bring more people into the blockchain gaming space and help them experience the benefits that blockchain gaming has to offer.
+                                    </p>
+                                    <p>Play to earn is one of the biggest steps in getting gamers hooked on a game.
+                                    </p>
+                                    <p>In addition to “play to earn”, Metal Roars offers other rewards in the form of coins that can be used to buy new trucks and parts for their truck. These rewards will keep the gamers coming back for more.</p>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="4">
+                                <Accordion.Header>What are the next major priorities in your roadmap? </Accordion.Header>
+                                <Accordion.Body>
+                                    <p>We are planning to launch a whole garage (Workshop) where the players will be able to upgrade their trucks, change their color and install new custom accessories.
+                                    </p>
+                                    <p>We are also planning to release new trucks, parts, maps, and game modes that will keep the gamers engaged. We will be working with the community to get their feedback and incorporating changes based on that feedback.
+                                    </p>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="5">
                                 <Accordion.Header>What are the different ways players can earn while they play?</Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua.
+                                    <p>There are three different ways that players can earn while they play:</p>
+                                    <ol>
+                                        <li>
+                                            <h5>Stunt to Earn:</h5>
+                                        </li>
+                                        <p>In this mode, the players have to perform stunts and get points for them. The more stunts they perform, the more points they will earn.
+                                        </p>
+                                        <li>
+                                            <h5>Damage to Earn:</h5>
+                                        </li>
+                                        <p>In this mode, the players have to damage their opponent's trucks and get points for it. The more damage they inflict, the more points they will earn.
+                                        </p>
+                                        <li>
+                                            <h5>Fight to earn:</h5>
+                                        </li>
+                                        <p>In this mode, the players have to battle it out with their opponents and get points for every hit they land. The more hits they land, the more points they will earn.
+                                            We believe that these modes will keep the gamers engaged and help them earn rewards while they play.
+                                        </p>
+                                    </ol>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="6">
+                                <Accordion.Header>What features or in-game assets make Metal roars a top 10 NFT game?</Accordion.Header>
+                                <Accordion.Body>
+                                    <p>Metal roars have one of the best P2E models ever placed in any NFT game. You can have multiple trucks, use them for your different fights depending on the game mode and even sell them on the open market.</p>
+                                    <p>In addition, we have a very strong community that is always willing to help out and a team that is always working on new game updates. We also have a very friendly in-game chat system where gamers can communicate with each other.</p>
+                                    <p>We believe that these features make Metal Roars a top 10 NFT game.</p>
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
