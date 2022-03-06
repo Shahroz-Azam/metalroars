b1aea60fc61155c163f24f702be35ab1ce53e02c
saad office <saadoffice@mail.com>
2022-03-06 20:53:21 +0500

    team description set

--- src/components/homepage/OurTeam.js
+++ src/components/homepage/OurTeam.js
@@ -4,12 +4,12 @@ import React from "react";
 import Ahmed from '../../assets/images/team/ahmed.jpeg'
 import Ars from '../../assets/images/team/arsalan.jpg'
 import Rao from '../../assets/images/team/rao.jpg'
-import Profile from '../../assets/images/team/avatar.gif'
+import Saad from '../../assets/images/team/saad.jpeg'
+import Waleed from '../../assets/images/team/waleed.jpeg'
 import Image from 'react-bootstrap/Image'
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
 export default function OurTeam({ history }) {
     return (
         <>
@@ -24,52 +24,52 @@ export default function OurTeam({ history }) {
                                 <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Ars} />
                             </Col>
                             <Col lg={8} md={12} xs={12}>
-                                <p className="memberDesignation">CEO</p>
-                                <p className="memberName">Vincent R</p>
+                                <p className="memberDesignation">Game Dev</p>
+                                <p className="memberName">Arslan Q</p>
                             </Col>
                         </Row>
                     </Col>
                     <Col lg={4} md={4} xs={6}>
                         <Row className="memberContainer">
                             <Col lg={4} md={12} xs={12}>
-                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Ars} />
+                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Rao} />
                             </Col>
                             <Col lg={8} md={12} xs={12}>
-                                <p className="memberDesignation">CEO</p>
-                                <p className="memberName">Vincent R</p>
+                                <p className="memberDesignation">CCO</p>
+                                <p className="memberName">MS Rao</p>
                             </Col>
                         </Row>
                     </Col>
                     <Col lg={4} md={4} xs={6}>
                         <Row className="memberContainer">
                             <Col lg={4} md={12} xs={12}>
-                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Ars} />
+                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Saad} />
                             </Col>
                             <Col lg={8} md={12} xs={12}>
-                                <p className="memberDesignation">CEO</p>
-                                <p className="memberName">Vincent R</p>
+                                <p className="memberDesignation">CMO</p>
+                                <p className="memberName">S Bhatti</p>
                             </Col>
                         </Row>
                     </Col>
                     <Col lg={4} md={4} xs={6}>
                         <Row className="memberContainer">
                             <Col lg={4} md={12} xs={12}>
-                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Ars} />
+                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Ahmed} />
                             </Col>
                             <Col lg={8} md={12} xs={12}>
-                                <p className="memberDesignation">CEO</p>
-                                <p className="memberName">Vincent R</p>
+                                <p className="memberDesignation">Junior Dev</p>
+                                <p className="memberName">Ahmed Q</p>
                             </Col>
                         </Row>
                     </Col>
                     <Col lg={4} md={4} xs={6}>
                         <Row className="memberContainer">
                             <Col lg={4} md={12} xs={12}>
-                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Ars} />
+                                <Image className="memberImage" alt="profile icon" loading="lazy" fluid={true} src={Waleed} />
                             </Col>
                             <Col lg={8} md={12} xs={12}>
-                                <p className="memberDesignation">CEO</p>
-                                <p className="memberName">Vincent R</p>
+                                <p className="memberDesignation">Web Dev</p>
+                                <p className="memberName">Waleed K</p>
                             </Col>
                         </Row>
                     </Col>
